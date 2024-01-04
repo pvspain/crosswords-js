76dd352c744ad5cfbf3c646aa39644c4e5e4da97
Paul Spain <pvspain@gmail.com>
2024-01-04 16:59:51 +1100

    Add new isX() helpers to module exports.

--- src/crosswords.js
+++ src/crosswords.js
@@ -11,23 +11,39 @@ import {
   newCrosswordDefinition,
   newCrosswordSource,
 } from "./crossword-model.mjs";
-import { assert, ecs, eid, trace, tracing } from "./helpers.mjs";
+import {
+  assert,
+  ecs,
+  eid,
+  isArray,
+  isNumber,
+  isObject,
+  isString,
+  isTruthy,
+  trace,
+  tracing,
+} from "./helpers.mjs";
 
 const helpers = {
   assert, // Validate function arguments and entry conditions
   ecs, // DOM helper, wrapper for document.getElementByClass()
   eid, // DOM helper, wrapper for document.getElementById()
+  isArray,
+  isNumber,
+  isObject,
+  isString,
+  isTruthy,
   trace, // Log information, warnings and errors to the console
   tracing, // Console logging toggle - pass boolean 'true' to emit messages to the console.
 };
 
 export {
-  newCrosswordSource, // Create a new minimal CrosswordSource string in JSON format.
-  newCrosswordDefinition, // Create a new CrosswordDefinition input and validate its structure.
-  convertSourceFileToDefinition, // Create a CrosswordDefinition from a filesystem path to a crossword source file (.json, .yml).
+  Controller, // Create a CrosswordDefinition from a filesystem path to a crossword source file (.json, .yml).
   // NOTE: This function accesses the local file system, and is therefore typically limited to server-side code.
-  compileCrossword, // Used by puzzle setters to verify the conformance of a crosswordDefinition
-  newCrosswordController, // Create a CrosswordController object - the central object in the module.
-  Controller, //Legacy export. Use newCrosswordController() for all new projects.
-  helpers, // A collection of non-essential utilities for argument validation, console-logging and DOM element retrieval
+  compileCrossword, // Create a new CrosswordDefinition input and validate its structure.
+  convertSourceFileToDefinition, //Legacy export. Use newCrosswordController() for all new projects.
+  helpers, // Used by puzzle setters to verify the conformance of a crosswordDefinition
+  newCrosswordController, // Create a new minimal CrosswordSource string in JSON format.
+  newCrosswordDefinition,
+  newCrosswordSource,
 };
