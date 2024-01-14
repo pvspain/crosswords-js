a85ef4cde2ef8ab1cf083bbe2b5d935f865a6947
Paul Spain <pvspain@gmail.com>
2024-01-14 14:29:07 +1100

    Fix githook for submdule scenario

--- bin/install-githook.sh
+++ bin/install-githook.sh
@@ -1,11 +1,17 @@
 #! /usr/bin/env bash
 
+# Change PWD to parent dir of this script (repository root)
+pushd $(dirname "$0")/.. >/dev/null
+
+# This works for standard repo or submodule
+gitDir=$(git rev-parse --git-dir)
+# Location of hookSource is relative to repo root
 hookSource=.githook-pre-commit
-hookTarget=.git/hooks/pre-commit
+hookTarget="$gitDir/hooks/pre-commit"
 backup=$hookTarget.backup-$(date --iso-8601=seconds)
 
-#  Test PWD is repo root
-if [[ -d .git ]] && [[ -f package.json ]]; then
+#  Test PWD is repo root and package root
+if ([[ -d .git ]] || [[ -f .git ]]) && [[ -f package.json ]]; then
     # Save existing
     if [[ -f "$hookTarget" ]]; then
         mv --force "$hookTarget" "$backup"
@@ -17,3 +23,5 @@ else
     printf "ERROR: This script must be run from the repository root directory\n"
     exit 1
 fi
+
+popd >/dev/null
